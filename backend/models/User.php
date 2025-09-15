cf7b0fbafc82c311b1b140f0b9c2664c1c6165db
uhuycihuy <ridhobujank69@gmail.com>
2025-09-15 10:36:38 +0700

    commit awal: backend, database.php, Pegawai.php dan User.php

---
+++ backend/models/User.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+
+class User {
+    private $conn;
+    private $table = "user";
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function login($username, $password) {
+        $query = "SELECT no_id, username, password FROM " . $this->table . " WHERE username = ? LIMIT 1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $username);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            return $user;
+        }
+
+        return false;
+    }
+
+    // Ambil semua user (opsional, buat manajemen user)
+    // public function getAll() {
+    //     $query = "SELECT id, username FROM " . $this->table;
+    //     $stmt = $this->conn->prepare($query);
+    //     $stmt->execute();
+    //     return $stmt;
+    // }
+}
+?>
